c5d3e22adfffd2da744abdfa11109220fecb5425
overclokk <info@overclokk.net>
2017-10-24 13:53:21 +0200

    Added the init files for the plugin

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Plugin Name: Plugin
+ * Description: WordPress Plugin Starter Boilerplate
+ * Plugin URI: https://italystrap.com
+ * Author: Enea Overclokk
+ * Author URI: https://italystrap.com 						
+ * Version: 1.0
+ * License: GPL2		
+ * Text Domain: plugin
+ * Domain Path: /lang
+ */
+
+/*
+
+    Copyright (C) 2017  Enea Overclokk  info@overclokk.net
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
